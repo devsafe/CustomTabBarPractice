08c9af8ddbabbf98b0a5a4a03339f09bc6b3378b
Rafinad <rafinad@outlook.com>
2021-05-08 21:29:16 +0300

    final design

--- CustomTabBarPractice/ContentView.swift
+++ CustomTabBarPractice/ContentView.swift
@@ -18,21 +18,22 @@ struct ContentView: View {
                         self.selected = 0
                     }) {
                         Image(systemName: "house")
-                    }.foregroundColor(self.selected == 0 ? .black : .white)
+                    }.foregroundColor(self.selected == 0 ? .black : .gray)
                     Spacer(minLength: 12)
                     Button(action: {
                         self.selected = 1
                     }) {
                         Image(systemName: "magnifyingglass")
-                    }.foregroundColor(self.selected == 1 ? .black : .white)
+                    }.foregroundColor(self.selected == 1 ? .black : .gray)
                     
                     Spacer().frame(width: 120)
                     
-                    Button(action: {
-                        self.selected = 2
-                    }) {
-                        Image(systemName: "person")
-                    }.foregroundColor(self.selected == 2 ? .black : .white)
+                        Button(action: {
+                            self.selected = 2
+                        }) {
+                            Image(systemName: "person")
+                        }.foregroundColor(self.selected == 2 ? .black : .gray)
+                        
                     
                     Spacer(minLength: 12)
                     
@@ -40,29 +41,49 @@ struct ContentView: View {
                         self.selected = 3
                     }) {
                         Image(systemName: "list.bullet")
-                    }.foregroundColor(self.selected == 3 ? .black : .white)
+                    }.foregroundColor(self.selected == 3 ? .black : .gray)
                 }
+                
                 .padding()
+                
                 .padding(.horizontal, 22)
-                .background(Color.pink)
-                                
+                
+                .background(Color.white)
                 Button(action: {
                     //
                 }) {
                     Image(systemName: "heart")
-                        .renderingMode(.original)
+                        //.renderingMode(.original)
+                        .font(.title)
                         .padding(28)
+                        .foregroundColor(.white)
                 }.background(Color.yellow)
                 .clipShape(Circle())
                 .offset(y: -32)
-                .shadow(color: Color.red,radius: 5)
+                .shadow(color: Color.white,radius: 45)
+
                 
             }
-        }
-        .background(Color.yellow.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
+        }.background(Color.yellow.edgesIgnoringSafeArea(.all))
+        
     }
 }
 
+struct CurvedShape: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x:0, y:0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 55))
+            
+            path.addArc(center: CGPoint(x: UIScreen.main.bounds.width / 2, y: 58), radius: 50, startAngle: .zero, endAngle: .init(degrees: 180), clockwise: true)
+            path.addLine(to: CGPoint(x: 0, y: 55))
+        }
+        .fill(Color.white)
+        .rotationEffect(.init(degrees: 180))
+        
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
