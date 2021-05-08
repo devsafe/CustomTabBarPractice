62b2905455cca9e90171571762de863461acfc7d
Rafinad <rafinad@outlook.com>
2021-05-08 19:51:30 +0300

    add icons to bottom

--- CustomTabBarPractice/ContentView.swift
+++ CustomTabBarPractice/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selected = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            ZStack {
+                HStack {
+                    Button(action: {
+                        self.selected = 0
+                    }) {
+                        Image(systemName: "house")
+                    }.foregroundColor(self.selected == 0 ? .black : .gray)
+                    Spacer(minLength: 12)
+                    Button(action: {
+                        self.selected = 1
+                    }) {
+                        Image(systemName: "magnifyingglass")
+                    }.foregroundColor(self.selected == 1 ? .black : .gray)
+                    
+                    Spacer().frame(width: 120)
+                    
+                    Button(action: {
+                        self.selected = 2
+                    }) {
+                        Image(systemName: "person")
+                    }.foregroundColor(self.selected == 2 ? .black : .gray)
+                    
+                    Spacer(minLength: 12)
+                    
+                    Button(action: {
+                        self.selected = 3
+                    }) {
+                        Image(systemName: "list.bullet")
+                    }.foregroundColor(self.selected == 3 ? .black : .gray)
+                }
+                .padding()
+                .padding(.horizontal, 22)
+            }
+        }
     }
 }
 
