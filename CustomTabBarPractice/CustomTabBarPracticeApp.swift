2c54ce620b2917fc251002e50fdfce17834c3fe7
Rafinad <rafinad@outlook.com>
2021-05-08 19:21:41 +0300

    Initial Commit

---
+++ CustomTabBarPractice/CustomTabBarPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarPracticeApp.swift
+//  CustomTabBarPractice
+//
+//  Created by Сахар Кубический on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
